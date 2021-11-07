ab3c8de1b424d6d706fb8bae7f665afaa7847a04
Zack Robertshaw <zackrobertshaw@gmail.com>
2021-11-07 15:47:56 -0500

    0% styling but all functions work HOLY S#?T

--- albums/models.py
+++ albums/models.py
@@ -6,8 +6,7 @@ from django.db.models.deletion import CASCADE
 class Albums(models.Model):
     title = models.CharField(max_length=100) 
     artist = models.CharField(max_length=100)
-    # created_at = models.DateField(null=True, blank=True, auto_now_add=True)
-
+    created_at = models.DateField(null=True, editable=False, blank=True, auto_now_add=True)
 
 
     """
